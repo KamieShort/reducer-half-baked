e500f1f45a40f2b73d2e5bb4a34e73f324a6767c
Kamie Short <kamie.short@gmail.com>
2022-05-11 16:48:34 -0700

    app functioning, colors changing

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState, useEffect } from 'react'
+import { useReducer, useEffect } from 'react'
 
 const colors = {
   pink: 'rgb(236, 72, 153)',
@@ -6,9 +6,11 @@ const colors = {
   red: 'rgb(239, 68, 68)',
 }
 
-const initialCountColor = { count: 0, currentColor: 'rgb(236, 72, 153)' }
+const initialCountColor = { count: 0, currentColor: colors.pink }
 
 function reduce(state, action) {
+  // console.log('state', state)
+  // console.log('action', action)
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1, currentColor: state.currentColor }
@@ -16,55 +18,41 @@ function reduce(state, action) {
     case 'decrement':
       return { count: state.count - 1, currentColor: state.currentColor }
     case 'reset':
-      return { count: 0 }
+      return { count: 0, currentColor: state.currentColor }
     case 'change':
-      return { count: state.count, color: state.currentColor }
+      return { count: state.count, currentColor: action.payload.color }
     default:
       throw new Error()
   }
 }
 
 export default function Counter() {
-  // const [count, setCount] = useState(0)
-  // const [currentColor, setCurrentColor] = useState(pinkRGB)
-  const [count, dispatch] = useReducer(reduce, initialCountColor)
+  const [countChange, dispatch] = useReducer(reduce, initialCountColor)
 
   useEffect(() => {
-    if (count === 0) {
-      initialCountColor(initialCountColor)
+    if (countChange.count === 0) {
+      dispatch({ type: 'change', payload: { color: colors.pink } })
     }
 
-    if (count > 0) {
-      initialCountColor(`rgb(52, 211, 153)`)
+    if (countChange.count > 0) {
+      dispatch({ type: 'change', payload: { color: colors.green } })
     }
 
-    if (count < 0) {
-      initialCountColor(`rgb(239, 68, 68)`)
+    if (countChange.count < 0) {
+      dispatch({ type: 'change', payload: { color: colors.red } })
     }
-  }, [count])
-
-  // const increment = () => {
-  //   setCount((prevState) => prevState + 1)
-  // }
-
-  // const decrement = () => {
-  //   setCount((prevState) => prevState - 1)
-  // }
-
-  // const reset = () => {
-  //   setCount(0)
-  // }
+  }, [countChange.count])
 
   return (
     <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
-      <h1 className="mb-5" style={{ color: initialCountColor }}>
-        {count.count}
+      <h1 className="mb-5" style={{ color: countChange.currentColor }}>
+        {countChange.count}
       </h1>
       <div className="flex w-1/2 justify-around">
         <button
           className="text-green-400 border-2 border-green-400 p-3"
           type="button"
-          onClick={() => dispatch({ type: 'increment', payload: initialCountColor })}
+          onClick={() => dispatch({ type: 'increment' })}
           aria-label="increment"
         >
           Increment
